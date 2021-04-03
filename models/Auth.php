177cae292e196eb28e24e25c0e1d8e68a3ddcffb
alexander <ryabyx.sasha@mail.ru>
2021-04-03 11:39:12 +0500

    add function register

--- models/Auth.php
+++ models/Auth.php
@@ -13,7 +13,31 @@ class Auth
     {
         $this->pdo=$pdo;
     }
-    public function auth($email,$password){
-        $stmt = $this->pdo->prepare()
+    public function auth($email,$password)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE email=:email");
+        $stmt->execute([
+            'email' => $email,
+        ]);
+        $user = $stmt->fetch();
+        if ($user){
+            if(password_verify($password,$user->password)) {
+                return $user;
+            }
+        }
+        return false;
+    }
+    public function register($nickname,$email,$password)
+    {
+        if($this->auth($email,$password)){
+            return -1;
+        }
+        $stmt=$this->pdo->prepare("INSERT INTO users (nickname,email,password) VALUES(:nickname,:email,:password)");
+        $stmt->execute([
+            'nickname'=>$nickname,
+            'email'=>$email,
+            'password'=>password_hash($password,PASSWORD_DEFAULT),
+        ]);
+        return $this->pdo->lastInsertId();
     }
 }
